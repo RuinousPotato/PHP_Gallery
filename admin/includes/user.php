43fd1800ce4ad98b3ffff3f2d61101eb833a2b2a
Ruinous Potato <fusiek@gmail.com>
2018-05-17 17:03:54 +0200

    user class finished

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -17,9 +17,20 @@ class User
 
   public static function find_user_by_id($id){
     global $database;
-    $result_set = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
-    $found_user = mysqli_fetch_array($result_set);
-    return $found_user;
+    $the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
+
+    return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+  //   if(!empty($the_result_array)){
+  //
+  //     $first_item = array_shift($the_result_array);
+  //
+  //     return first_item;
+  //   } else {
+  //     return false;
+  //   }
+  //
+  //   return $found_user;
   }
 
   public static function find_this_query($sql){
