6a852d92e73ab9112a907128190254e781f2f2d4
Ruinous Potato <fusiek@gmail.com>
2018-05-18 12:16:31 +0200

    session class added

---
+++ admin/includes/session.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ */
+class ClassName
+{
+
+  private $signed_in;
+  public  $user_id;
+
+  function __construct(argument)
+  {
+    session_start();
+  }
+
+
+}
+$session = new Session();
+
+ ?>
