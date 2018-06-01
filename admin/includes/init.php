d1f161dab6f08fa7a65d8d5df3fe7bda4e849c40
Ruinous Potato <fusiek@gmail.com>
2018-06-01 23:43:31 +0200

    missing dot added to file path

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -2,7 +2,7 @@
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-define('SITE_ROOT', 'C:\xampp\htdocs\gallery');//'C:' . DS . 'xampp' DS . 'htdocs' . DS . 'gallery');
+define('SITE_ROOT', 'C:' . DS . 'xampp' . DS . 'htdocs' . DS . 'gallery');  //'C:\xampp\htdocs\gallery'
 
 defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
 
