7cf2555e67363f255802385f1329661199dafaa6
Ruinous Potato <fusiek@gmail.com>
2018-06-18 22:54:42 +0200

    edit photo page form info added

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -1,10 +1,26 @@
 <?php
   include("includes/header.php");
   if (!$session->is_signed_in()) {redirect("login.php");}
+
   $photos = Photo::find_all();
-  if(isset($_POST['update'])){
-    echo "it works";
-  };
+
+  if(empty($_GET['id'])){
+    redirect("photos.php");
+  } else {
+    $photo = Photo::find_by_id($_GET['id']);
+    if(isset($_POST['update'])){
+      if($photo){
+        $_POST['title'];
+        $_POST['caption'];
+        $_POST['alternate_text'];
+        $_POST['description'];
+      }
+    }
+  }
+
+
+
+
 ?>
 
 
