81d88edf140c80563d7b28ac1117dea9ca0b5311
Ruinous Potato <fusiek@gmail.com>
2018-06-18 22:38:51 +0200

    photo details column added

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -2,6 +2,9 @@
   include("includes/header.php");
   if (!$session->is_signed_in()) {redirect("login.php");}
   $photos = Photo::find_all();
+  if(isset($_POST['update'])){
+    echo "it works";
+  };
 ?>
 
 
@@ -29,23 +32,59 @@
                         EDIT
                         <small>Subheading</small>
                     </h1>
-                    <div class="col-lg-8">
-                      <div class="form-group">
-                        <input type="text" name="title" class="form-control" value="">
+                    <form class="" action="edit_photo.php" method="post">
+                      <div class="col-lg-8">
+                        <div class="form-group">
+                          <input type="text" name="title" class="form-control" value="">
+                        </div>
+                        <div class="form-group">
+                          <label for="caption">Caption</label>
+                          <input type="text" name="caption" class="form-control" value="">
+                        </div>
+                        <div class="form-group">
+                          <label for="caption">Alternate Text</label>
+                          <input type="text" name="alternate_text" class="form-control" value="">
+                        </div>
+                        <div class="form-group">
+                          <label for="caption">Description</label>
+                          <textarea class="form-control" name="description" rows="10" cols="30"></textarea>
+                        </div>
                       </div>
-                      <div class="form-group">
-                        <label for="caption">Caption</label>
-                        <input type="text" name="caption" class="form-control" value="">
+                      <div class="col-md-4" >
+                        <div  class="photo-info-box">
+                          <div class="info-box-header">
+                            <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                          </div>
+                          <div class="inside">
+                            <div class="box-inner">
+                              <p class="text">
+                                <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                              </p>
+                              <p class="text ">
+                                Photo Id: <span class="data photo_id_box">34</span>
+                              </p>
+                              <p class="text">
+                                Filename: <span class="data">image.jpg</span>
+                              </p>
+                              <p class="text">
+                                File Type: <span class="data">JPG</span>
+                              </p>
+                              <p class="text">
+                                File Size: <span class="data">3245345</span>
+                              </p>
+                            </div>
+                            <div class="info-box-footer clearfix">
+                              <div class="info-box-delete pull-left">
+                                <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>
+                              </div>
+                              <div class="info-box-update pull-right ">
+                                <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div class="form-group">
-                        <label for="caption">Alternate Text</label>
-                        <input type="text" name="alternate_text" class="form-control" value="">
-                      </div>
-                      <div class="form-group">
-                        <label for="caption">Description</label>
-                        <textarea class="form-control" name="description" rows="10" cols="30"></textarea>
-                      </div>
-                    </div>
+                    </form>
                 </div>
               </div>
               <!-- /.row -->
